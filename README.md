34b0554f67fc1dab1b0870efc70c4f63bf32bfab
Fernando Remde <fernandoremde@Fernandos-MacBook-Pro.local>
2020-05-08 17:18:05 -0300

    improved login screen responsiveness

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-You can view this project [clicking here](http://161.35.136.196/).
+You can see the project in its current state in this link: [161.35.136.196](http://161.35.136.196/).<br />
 
-Gardn is a web message app based on React (Javascript, HTML, CSS, Node.js) made by [Fernando Remde](https://www.github.com/Remde) and [Bruno Moraes](https://www.github.com/brunosdm). <br />To start it in development mode, run:
+Gardn is an anonymous web message app based on React (Javascript, HTML, CSS, Node.js) made by [Fernando Remde](https://www.github.com/Remde) and [Bruno Moraes](https://www.github.com/brunosdm). It's deployed to our Linux remote server using nginx for the front-end and pm2 for the back-end.<br />To start it in development mode, run the client with:
 
 ### `npm start`
 
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+And the server with:
+
+### `node server.js`
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser. You might need to install the dependencies with npm i both for client and server.
 
 <br />
 Work in progress.
--- client/src/Components/login.js
+++ client/src/Components/login.js
@@ -30,30 +30,36 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div className="login">
-        <section className="masthead" role="img" aria-label="Image Description">
-          <h1>Gardn.</h1>
-          <p>Converse anonimamente.</p>
-          <form onSubmit={this.onFormSubmit}>
-            <div className="nameInput">
+      <div className="ui container">
+        <div className="login">
+          <section
+            className="masthead"
+            role="img"
+            aria-label="Image Description"
+          >
+            <h1>Gardn.</h1>
+            <p>Converse anonimamente.</p>
+            <form onSubmit={this.onFormSubmit}>
+              <div className="nameInput">
+                <input
+                  type="text"
+                  value={this.state.term}
+                  onChange={(e) => this.setState({ term: e.target.value })}
+                  placeholder="Insira seu codinome"
+                  required
+                  title="1 a 20 dígitos sem caracteres especiais"
+                ></input>
+              </div>
               <input
-                type="text"
-                value={this.state.term}
-                onChange={(e) => this.setState({ term: e.target.value })}
-                placeholder="Insira seu codinome"
-                required
-                title="1 a 20 dígitos sem caracteres especiais"
+                type="button"
+                className="link-button"
+                onClick={this.onButtonClick}
+                value="Entrar"
+                pattern="[A-Za-z0-9]{1,20}"
               ></input>
-            </div>
-            <input
-              type="button"
-              className="link-button"
-              onClick={this.onButtonClick}
-              value="Entrar"
-              pattern="[A-Za-z0-9]{1,20}"
-            ></input>
-          </form>
-        </section>
+            </form>
+          </section>
+        </div>
       </div>
     );
   }
